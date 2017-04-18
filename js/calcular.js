6e924a43d8718ea9ec055445a1ebe206206b8a68
Victor Ferreira <contato.blitzplay@gmail.com>
2017-04-18 17:06:52 -0300

    primeira versão do calculo imc

---
+++ js/calcular.js
@@ -0,0 +1,7 @@
+function calcula(){
+    var peso = parseFloat(document.getElementById = "peso");
+    var altura = parseFloat(document.getElementById = "altura");
+    var imc;
+    imc = peso / (altura * 2);
+    console.log(imc);
+}
